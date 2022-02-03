906ae988eb3ae29826a35f82b44a920cabfbffdc
Woosang <yws1502@naver.com>
2022-02-03 11:55:03 +0900

    ✨ movie api 연결 (apikey는 일단 삭제)

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo"
 
-export default function Home() {
+const ENDPOINT = "https://api.themoviedb.org/3/";
+
+export default function Home () {
+  const [ movies, setMovies ] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(`${ENDPOINT}movie/popular?api_key=${API_KEY}`)
+      ).json();
+      setMovies(results)
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Home</h1>
+      <h1 className="active">Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      { movies?.map((movie) => {
+        return (
+          <div key={ movie.id }>
+            <h4>{ movie.original_title }</h4>
+          </div>
+        )
+      })}
     </div>
   )
 }
